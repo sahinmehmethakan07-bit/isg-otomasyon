da5be9b601c1bb9038062be8c91c1a66669308ef
Mehmet Hakan Sahin <mehmethakansahin@mbpvonmehmet2.speedport.ip>
2026-05-18 13:33:30 +0200

    fix: framer-motion onDrag tip uyuşmazlığı ModernInput için giderildi

--- lib/ModernInput.tsx
+++ lib/ModernInput.tsx
@@ -66,7 +66,7 @@ export function ModernInput({
             focus:outline-none
             ${className}
           `}
-          {...props}
+          {...(props as any)}
         />
       </div>
 
